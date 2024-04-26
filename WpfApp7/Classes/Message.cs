a63f0b3a6bf7f9c047c3aaead42736dbc0bd3134
Muhammad <maga168123@gmail.com>
2024-04-26 19:06:57 +0400

    Azeri Chat

---
+++ WpfApp7/Classes/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7.Classes
+{
+    public class Message
+    {
+        public DateTime date { get; set; }
+        public string Title { get; set; }
+
+        public Message(string title)
+        {
+            this.date = DateTime.Now;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{date.ToShortTimeString()}                {Title}";
+        }
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WpfApp7.Classes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private ObservableCollection<Message> message;
+
+        public ObservableCollection<Message> msgs
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged(); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            msgs = new ObservableCollection<Message>
+            {
+                new Message("Salam")
+            };
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(MyTextBox.Text != string.Empty)
+            {
+                msgs.Add(new Message(MyTextBox.Text));
+                MyTextBox.Text = null;
+            }
+        }
+    }
+}
